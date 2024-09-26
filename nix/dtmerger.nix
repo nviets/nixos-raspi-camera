00e209485dbb0e27e3f7a307cca427954abac26e
Sergei Mironov <sergei.v.mironov@proton.me>
2024-09-26 20:22:26 +0000

    Add dtmerger

---
+++ nix/dtmerger.nix
@@ -0,0 +1,11 @@
+{ stdenv
+, lib
+, writeShellScript
+, libraspberrypi
+} :
+
+writeShellScript "dtmerger.sh" ''
+  O=${./ov5647.dtbo}
+  D=$(dirname $O)
+  exec ${libraspberrypi}/bin/dtoverlay -d $D $O
+''
