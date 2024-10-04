13463ca44bfdce8de691baabc3ea01c315d0a718
Sergei Mironov <sergei.v.mironov@proton.me>
2024-10-04 10:48:36 +0300

    Add rtsp server service

--- nix/raspi-camera.nix
+++ nix/raspi-camera.nix
@@ -20,17 +20,33 @@ with lib;
     };
   in {
 
-    systemd.services.tweaks = {
-      script = ''
-        ${dtmerger}/bin/dtmerger.sh
-      '';
-      wantedBy = [ "multi-user.target" ];
-    };
-
     services.udev = {
       extraRules = ''
         SUBSYSTEM=="dma_heap", GROUP="video", MODE="0660"
       '';
+    };
+
+    systemd.services.dtmerger = {
+      script = ''
+        ${dtmerger}/bin/dtmerger.sh
+      '';
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+    };
+
+    systemd.services.rpicam-rtsp-server = {
+      script = with pkgs; ''
+        ${rpicam-apps}/bin/rpicam-vid -t 0 --inline -o - \
+          --hflip --vflip --width 320 --height 240 \
+        | ${vlc}/bin/cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream1}' \
+          :demux=h264 \
+        >/dev/null 2>&1
+      '';
+      after = [ "dtmerger.service" ];
+      wantedBy = [ "multi-user.target" ];
     };
 
     nixpkgs.config.permittedInsecurePackages = [
