d61078353a20c970f65645ae3200495d26735ac0
Sergei Mironov <sergei.v.mironov@proton.me>
2024-10-05 15:06:37 +0300

    Add packages.nix

---
+++ nix/packages.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+
+  libpisp = pkgs.callPackage ./libpisp.nix {};
+  libcamera = pkgs.callPackage ./libcamera-raspi.nix {
+    inherit libpisp;
+  };
+  dtmerger = pkgs.callPackage ./dtmerger.nix {};
+  rpicam-apps = pkgs.callPackage ./rpicam-apps.nix {
+    inherit libpisp libcamera;
+  };
+
+}
